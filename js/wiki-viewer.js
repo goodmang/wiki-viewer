25b13a6fd77efa34884ba77dae0eb7e7c85fd9a3
Greg Goodman <greg@goodmandigital.com>
2017-03-22 12:53:44 -0700

    Get text first

--- js/wiki-viewer.js
+++ js/wiki-viewer.js
@@ -1,11 +1,54 @@
-//'use strict';
+'use strict';
 
+
+function searchArticlesV2() {
+    var searchString = $("#search").val();
+    $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php',
+        data: {
+            "action": "query",
+            "format": "json",
+            "prop": "info",
+            "list": "search",
+            "meta": "",
+            "formatversion": "2",
+            "origin": "*",
+            "srsearch": searchString
+        },
+        dataType: 'json',
+        method: "GET",
+        success: function(result) {
+
+            var html = '';
+            var snippetStr;
+
+            for (var i = 0; i < result.query.search.length; i++) {
+                snippetStr = undefined;
+                if (result.query.search[i].hasOwnProperty("snippet")) {
+                    snippetStr = result.query.search[i].snippet
+                }
+                html += makeArticleCard({
+                    title: result.query.search[i].title,
+                    snippet: snippetStr,
+                });
+            }
+
+            var elem = document.getElementById('searchResults');
+            elem.innerHTML = html;
+        },
+        error: function(textStatus, errorThrown) {
+            console.log('Error:');
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+}
 
 
 function searchArticles() {
     var searchString = $("#search").val();
     $.ajax({
-      url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&list=&meta=&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=300&pilimit=10&wbptterms=description&gpslimit=10&gpssearch=' + encodeURI(searchString + "&origin=*"),
+        url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&list=&meta=&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=300&pilimit=10&wbptterms=description&gpslimit=10&gpssearch=' + encodeURI(searchString + "&origin=*"),
         //     url: 'https://en.wikipedia.org/w/api.php',
         //     data: {
         //     //   action: "query",
@@ -37,14 +80,14 @@ function searchArticles() {
             for (var i = 0; i < result.query.pages.length; i++) {
                 snippetStr = undefined;
                 if (result.query.pages[i].hasOwnProperty("terms")) {
-                  if (result.query.pages[i].terms.hasOwnProperty("description")) {
-                    snippetStr = result.query.pages[i].terms.description
-                  }
+                    if (result.query.pages[i].terms.hasOwnProperty("description")) {
+                        snippetStr = result.query.pages[i].terms.description
+                    }
                 }
                 html += makeArticleCard({
                     title: result.query.pages[i].title,
                     snippet: snippetStr,
-                    image: (result.query.pages[i].hasOwnProperty("thumbnail"))?result.query.pages[i].thumbnail.source:undefined
+                    image: (result.query.pages[i].hasOwnProperty("thumbnail")) ? result.query.pages[i].thumbnail.source : undefined
                 });
             }
 
@@ -60,19 +103,20 @@ function searchArticles() {
 }
 
 function createLink(title) {
-  // Creates a link to the article based on the title
-  return "https://en.wikipedia.org/wiki/" + encodeURI(title);
+    // Creates a link to the article based on the title
+    return "https://en.wikipedia.org/wiki/" + encodeURI(title);
 }
+
 function makeArticleCard(article) {
     var html = '<div class="card article">';
 
-    if (article.image) {
-      html += '<img class="card-img-top img-fluid" src="' + article.image + '" alt="Image of ' + article.title + '">';
-    }
+    // if (article.image) {
+    //     html += '<img class="card-img-top img-fluid" src="' + article.image + '" alt="Image of ' + article.title + '">';
+    // }
     html += '<div class="card-block">';
     html += '<h4 class="card-title">' + article.title + '</h4>';
     if (article.snippet) {
-      html += '<p class="card-text">' + article.snippet + '</p>';
+        html += '<p class="card-text">' + article.snippet + '</p>';
     }
     html += '<a href="' + createLink(article.title) + '" class="card-link">Read Article</a>';
     html += '</div></div>'
@@ -81,5 +125,5 @@ function makeArticleCard(article) {
 
 
 $(document).ready(function() {
-  $("#search").on("search", searchArticles);
+    $("#search").on("search", searchArticlesV2);
 });
